1a416885bfb6010b6a11a5c6b42fe3d6a5ab5c14
PatrickKibe <phantickibe01@gmail.com>
2022-06-17 21:10:38 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // iterate from the beginning to the middle
+  for (let i = 0; i < word.length / 2; i++) {
+    // check each letter to the correponding letter from the end
+    const j = word.length - 1 - i;
+    // if any letters don't match, return false
+    if (word[i] !== word[j]) return false;
+  }
+
+  // return true
+  return true;
+}
+
+/* 
+r a c e c a r
+0 1 2 3 4 5 6
+i           j
+r a c e c a r
+0 1 2 3 4 5 6
+  i       j
+r a c e c a r
+0 1 2 3 4 5 6
+    i   j
+r a c e c a r
+0 1 2 3 4 5 6
+      ij
 }
 
 /* 
@@ -9,7 +34,22 @@ function isPalindrome(word) {
 /*
   Add written explanation of your solution here
 */
+// You can run `node index.js` to view these console logs
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+  console.log("");
 
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+}
+module.exports = isPalindrome;
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
@@ -21,5 +61,4 @@ if (require.main === module) {
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
-
 module.exports = isPalindrome;
